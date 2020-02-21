429eb2605fa9ba91dbb955c6bb8dbdd2ed5266e7
Nicole Chinn <nychinn@gmail.com>
2020-02-21 14:06:06 +1300

    continue tutorial

--- src/screens/ResultsShowScreen.js
+++ src/screens/ResultsShowScreen.js
@@ -1,14 +1,57 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, Image, StyleSheet, FlatList } from 'react-native';
+import yelp from '../api/yelp';
 
-const ResultsShowScreen = () => {
+const ResultsShowScreen = ({ navigation }) => {
+    const [result, setResult] = useState(null);
+    const id =  navigation.getParam('id');
+
+    const getResult = async (id) => {
+        const response = await yelp.get(`/${id}`);
+        setResult(response.data);
+    };
+
+    useEffect(() => {
+        getResult(id);
+    }, []);
+
+    if ( !result ) {
+        return null;
+    }
+
     return(
-        <View>
-            <Text>Results Show</Text>
+        <View style={styles.container}>
+            <Text style={styles.name}>{result.name}</Text>
+            <FlatList
+                data={result.photos}
+                keyExtractor={(photo) => photo}
+                renderItem={({item}) => {
+                    return <Image style={styles.image} source={{ uri: item }}/>
+                }}
+            />
         </View>
     );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    container: {
+        paddingHorizontal: 16,
+        paddingTop: 16,
+        alignItems: 'center',
+        flex: 1
+    },
+    name: {
+        fontSize: 16,
+        fontWeight: 'bold',
+        marginBottom: 16,
+        textAlign: 'center'
+    },
+    image: {
+        height: 200,
+        width: 300,
+        marginBottom: 10,
+        borderRadius: 4
+    }
+});
 
 export default ResultsShowScreen;
